d30bbc05ddd993677fbd638b4128971ec171bace
DylanPagr95 <34363720+DylanPagr95@users.noreply.github.com>
2017-12-09 00:12:36 +0800

    Add files via upload

---
+++ GameEngine/src/shaders/StaticShader.java
@@ -0,0 +1,16 @@
+package shaders;
+
+public class StaticShader extends ShaderProgram {
+
+	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
+	private static final String FAGMENT_FILE = "src/shaders/fragmentShader.txt";
+
+	public StaticShader() {
+		super(VERTEX_FILE, FAGMENT_FILE);
+	}
+
+	protected void bindAttributes() {
+		super.bindAttribute(0, "position");
+	}
+
+}
